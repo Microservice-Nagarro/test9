4ac9274cf5a69e13833869d5836eee13cb0de981
Paweł Tichoniuk <pawel.tichoniuk.ext@nagarro.com>
2024-08-14 17:28:45 +0200

    PLA-414 - further unit tests

--- tests/BHF.MS.MyMicroservice.Database.Tests/Services/DbItemServiceTests.cs
+++ tests/BHF.MS.MyMicroservice.Database.Tests/Services/DbItemServiceTests.cs
@@ -1,8 +1,10 @@
 using System.Globalization;
 using BHF.MS.MyMicroservice.Database.Context;
 using BHF.MS.MyMicroservice.Database.Context.Entities;
+using BHF.MS.MyMicroservice.Database.Models.DbItem;
 using BHF.MS.MyMicroservice.Database.Services;
 using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using Moq.EntityFrameworkCore;
 
@@ -41,6 +43,87 @@ namespace BHF.MS.MyMicroservice.Database.Tests.Services
 
             var minLastUpdated = entityList.MinBy(x => x.LastUpdated);
             result.Should().NotContain(x => x.Id == minLastUpdated!.Id);
+        }
+
+        [Fact]
+        public async Task GetById_WhenNotFound_ReturnsNull()
+        {
+            // Arrange
+            _contextMock.SetupGet(x => x.DbItems).ReturnsDbSet([]);
+
+            // Act
+            var result = await _sut.GetById(Guid.NewGuid());
+
+            // Assert
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task GetById_WhenFound_ReturnsItem()
+        {
+            // Arrange
+            var dbItem = new DbItem { Id = Guid.NewGuid() };
+            _contextMock.SetupGet(x => x.DbItems).ReturnsDbSet([dbItem]);
+
+            // Act
+            var result = await _sut.GetById(dbItem.Id);
+
+            // Assert
+            result.Should().NotBeNull();
+            result!.Id.Should().Be(dbItem.Id);
+        }
+
+        [Fact]
+        public async Task Update_WhenNotFound_ReturnsFalse_StopsFurtherExecution()
+        {
+            // Arrange
+            _contextMock.SetupGet(x => x.DbItems).ReturnsDbSet([]);
+
+            // Act
+            var result = await _sut.Update(new DbItemDto { Id = Guid.NewGuid() });
+
+            // Assert
+            result.Should().BeFalse();
+            _contextMock.Verify(x => x.SaveChangesAsync(default), Times.Never);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async Task Update_WhenDbUpdateConcurrencyException_ReturnsFalse(bool itemFound)
+        {
+            // Arrange
+            var dbItem = new DbItem { Id = Guid.NewGuid() };
+            var model = new DbItemDto(dbItem);
+
+            _contextMock.SetupGet(x => x.DbItems).ReturnsDbSet(itemFound ? [dbItem] : []);
+            _contextMock.Setup(x => x.DbItems.FindAsync(dbItem.Id))
+                .ReturnsAsync(dbItem);
+
+            _contextMock.Setup(x => x.SaveChangesAsync(default)).Throws<DbUpdateConcurrencyException>();
+
+            // Act
+            var result = await _sut.Update(model);
+
+            // Assert
+            result.Should().Be(itemFound);
+        }
+
+        [Fact]
+        public async Task Update_WhenRandomExceptionThrown_ShouldNotCatchException()
+        {
+            // Arrange
+            var dbItem = new DbItem { Id = Guid.NewGuid() };
+            var model = new DbItemDto(dbItem);
+            _contextMock.Setup(x => x.DbItems.FindAsync(dbItem.Id))
+                .ReturnsAsync(dbItem);
+            _contextMock.Setup(x => x.SaveChangesAsync(default)).Throws<Exception>();
+
+            // Act
+            var act = () => _sut.Update(model);
+
+            // Assert
+            await act.Should().ThrowAsync<Exception>();
         }
     }
 }
