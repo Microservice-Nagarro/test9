fc55d2d21bfc60af05a26fac2f6c51176a748315
Paweł Tichoniuk <pawel.tichoniuk.ext@nagarro.com>
2024-08-16 17:02:52 +0200

    PLA-414 - further coverage fixes

--- tests/BHF.MS.MyMicroservice.Tests/Services/ExampleServiceTests.cs
+++ tests/BHF.MS.MyMicroservice.Tests/Services/ExampleServiceTests.cs
@@ -35,7 +35,7 @@ namespace BHF.MS.MyMicroservice.Tests.Services
             _httpMessageHandlerMock
                 .SetupRequest(HttpMethod.Post,
                     new Uri(_httpClient.BaseAddress!, _settings.Value.Endpoint1Uri))
-                .ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK), TimeSpan.FromSeconds(1));
+                .ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK), TimeSpan.FromMilliseconds(100));
 
             // Act
             var result = await _sut.DoSomethingAsync(model);
